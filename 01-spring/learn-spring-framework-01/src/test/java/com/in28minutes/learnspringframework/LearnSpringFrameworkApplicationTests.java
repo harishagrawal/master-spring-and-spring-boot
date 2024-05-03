c6a11c158f7f95bec3969903874607237d089773
Harish Agrawal <harish@zb.io>
2024-05-03 16:57:13 +0530

    Changes to Java version for coverage report

--- 01-spring/learn-spring-framework-01/src/test/java/com/in28minutes/learnspringframework/LearnSpringFrameworkApplicationTests.java
+++ 01-spring/learn-spring-framework-01/src/test/java/com/in28minutes/learnspringframework/LearnSpringFrameworkApplicationTests.java
@@ -1,13 +1,9 @@
 package com.in28minutes.learnspringframework;
 
-import org.junit.jupiter.api.Test;
+// import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class LearnSpringFrameworkApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
